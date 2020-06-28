15aab4c733a4a54318115365b366b3d66daf2247
FraserMuir <frasermuir14@gmail.com>
2020-06-28 21:43:46 +0100

    Add Analytics

---
+++ gatsby-ssr.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  runPostHogAnalytics(setHeadComponents);
+};
+
+const API_KEY = "EHrPz_F_1WP0_b_d_KCCul_zsyeXp7WKAOfl9cAo-6M";
+const API_HOST = "https://app.posthog.com";
+
+// Initialises PostHog Analytics via a script injected into the html head
+const runPostHogAnalytics = (setHeadComponents) => {
+  // if (process.env.NODE_ENV !== "production") return null;
+
+  const posthogScript = (
+    <script
+      key="posthog-analytics"
+      dangerouslySetInnerHTML={{
+        __html: `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+                 posthog.init('${API_KEY}', {api_host: '${API_HOST}'})`,
+      }}
+    />
+  );
+
+  setHeadComponents([posthogScript]);
+};
