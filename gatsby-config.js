32ff8a14fce292f15b3e6361db22ee6933ea95ca
FraserMuir <frasermuir14@gmail.com>
2020-07-04 17:41:31 +0100

    re-add force trailing slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-force-trailing-slashes",
     {
       resolve: "gatsby-plugin-scroll-indicator",
       options: {
